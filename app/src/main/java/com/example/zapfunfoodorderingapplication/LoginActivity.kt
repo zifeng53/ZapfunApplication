39b84526b2783693f45eb72fe6f239fe3afb6458
yeoww <yeowweilun97@gmail.com>
2020-08-30 11:57:40 +0800

    login register firebase yi dian dian commited by weilun

---
+++ app/src/main/java/com/example/zapfunfoodorderingapplication/LoginActivity.kt
@@ -0,0 +1,39 @@
+package com.example.zapfunfoodorderingapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        auth = Firebase.auth
+
+        noaccount.setOnClickListener {
+            startActivity(Intent(this,RegisterAccountActivity::class.java))
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    fun updateUI(currentUser : FirebaseUser?) {
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/zapfunfoodorderingapplication/RegisterAccountActivity.kt
@@ -0,0 +1,60 @@
+package com.example.zapfunfoodorderingapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_register_account.*
+import java.util.regex.Pattern
+
+class RegisterAccountActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_account)
+        auth = Firebase.auth
+
+        registerbtn.setOnClickListener {
+
+        }
+    }
+
+    private fun registerUser() {
+        if(email.text.toString().isEmpty()) {
+            email.error = "Please enter email"
+            email.requestFocus()
+            return
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) {
+            email.error = "Please enter email"
+            email.requestFocus()
+            return
+        }
+
+        if(password.text.toString().isEmpty()) {
+            password.error = "Please enter password"
+            password.requestFocus()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    startActivity(Intent(this,LoginActivity::class.java))
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Register failed. Try again after some time.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
