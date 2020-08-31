b1530235c9e25875b00ea69003db6328978a835e
PhilipHuan <philip5652@live.com>
2020-08-31 23:42:30 +0800

    finish toolbar & 30% done drawer menu -- Philip

--- app/src/main/java/com/example/zapfunfoodorderingapplication/MainActivity.kt
+++ app/src/main/java/com/example/zapfunfoodorderingapplication/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.zapfunfoodorderingapplication
 
+import android.app.Notification
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.isVisible
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.appbar.*
 import kotlinx.android.synthetic.main.fragment_my_history.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     // create three objects for fragments
     /*lateinit var myMenuFragment: MyMenuFragment
@@ -25,19 +36,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //appbar top start
+        //ToolBar.visibility = View.GONE
+        setSupportActionBar(ToolBar)
+
+
+        /*val toolbarView = findViewById<Toolbar>(R.id.ToolBar)
+        val toolbarController = findNavController(R.id.myNavHostFragment)
+        ToolBar.setupWithNavController(toolbarController)
+        fun showToolbar() {
+            ToolBar.visibility = View.VISIBLE
+        }
+        fun hideToolbar() {
+            ToolBar.visibility = View.GONE
+        }
+        toolbarController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.myMenuFragment -> showToolbar()
+                else -> hideToolbar()
+            }
+        }*/
+
+
+
+
+
+        //btm_nav start
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.btm_nav)
         val navController = findNavController(R.id.myNavHostFragment)
-
         bottomNavigationView.setupWithNavController(navController)
-
         fun showBottomNav() {
             btm_nav.visibility = View.VISIBLE
         }
-
         fun hideBottomNav() {
             btm_nav.visibility = View.GONE
         }
-
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.myMenuFragment -> showBottomNav()
@@ -46,7 +79,7 @@ class MainActivity : AppCompatActivity() {
                 else -> hideBottomNav()
             }
         }
-
+        //btm_nav end
 
 
 
@@ -102,6 +135,10 @@ class MainActivity : AppCompatActivity() {
             true
         }*/
 
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return true
     }
 
 }
